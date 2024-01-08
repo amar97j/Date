a5d3c8f4bc3217f1280ab07785a0a53822667278
Amar J M H M H  Alfailakawi <83320@kfh.com>
2024-01-08 14:24:17 +0300

    Date

---
+++ Dates/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) {
+        Date today = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(today);
+        calendar.add(Calendar.DAY_OF_MONTH, 5); // Adding 5 days
+
+        Date futureDate = calendar.getTime();
+        System.out.println("Date after 5 days: " + futureDate);
+
+        Date date1 = new Date(); // 15th Feb 2023
+        Date date2 = new Date(); // 20th Feb 2023
+
+        boolean isBefore = date1.before(date2);
+        System.out.println("Is date1 before date2? " + isBefore);
+    }
+}
